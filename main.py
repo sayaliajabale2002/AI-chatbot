b4aee4fdab6ef1b7ea19484c01ba8ccae47790bd
Sayali Ajabale <ajabalesayali30@gmail.com>
2026-08-21 17:15:22 +0530

    Refactor chatbot with tools and update query handling

--- main.py
+++ main.py
@@ -1,45 +1,64 @@
 import streamlit as st 
 from langchain.chat_models import init_chat_model
-from typing import TypedDict,Annotated
 from dotenv import load_dotenv
-from langchain.messages import AnyMessage, SystemMessage, HumanMessage, AIMessage
+from typing import TypedDict,Annotated
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, ToolMessage,AnyMessage
 from langgraph.graph.message import add_messages
-from langgraph.graph import StateGraph, START, END
+from langgraph.graph import START, END, StateGraph
+from langgraph.prebuilt import ToolNode,tools_condition
 from langgraph.checkpoint.memory import MemorySaver
 
+from tavily import TavilyClient
+import os 
+api_key = os.getenv('TAVILY_API_KEY')
+
 load_dotenv()
-st.title("💬 LangGraph Chatbot")
-llm_model = init_chat_model(model="gemini-2.5-flash",model_provider="google_genai")
+st.title("💬 Chatbot")
+llm_model = init_chat_model(model="gemini-2.5-flash",model_provider="google_genai",temperature=0.1)
 
 class State(TypedDict):
-    human_message : Annotated[list[HumanMessage],add_messages]
-    ai_message: Annotated[list[AIMessage],add_messages]
+    messages: Annotated[list[AnyMessage],add_messages]
 
-def chatmodel(state: State):
-    prompt = "you are an assistant who will assist user"
-    resposne = llm_model.invoke([SystemMessage(content=prompt)] + state["human_message"])
-    return {"ai_message" : resposne}
+def sum(a:int,b:int)->int:
+    '''addition of a and b'''
+    res = a+b
+    return f'{a} + {b} = {res}'
 
-# @st.cache_resource
+def search_tool(state:State):
+    '''answer user query on real time web search data'''
+    client = TavilyClient(api_key)
+    response = client.search(
+        query=state['messages'][-1].content,
+        # search_depth="advanced",
+        max_results=2
+    )
+    search_result_text = str(response) 
+    return {'messages': [AIMessage(content=search_result_text)]}
+
+tools = [sum, search_tool]
+llm_with_tool = llm_model.bind_tools(tools)
+
+def chatbot(state:State):
+    '''You are a chatbot who will answer user query'''
+    res = llm_with_tool.invoke(state['messages'])
+    return {'messages':[res]}
+
 def get_agent():
     memory = MemorySaver()
     graph = StateGraph(State)
-    graph.add_node("chatmodel", chatmodel)
-    graph.add_edge(START, "chatmodel")
-    graph.add_edge("chatmodel", END)
-    return graph.compile(checkpointer= memory)
+    graph.add_node('chatbot',chatbot)
+    graph.add_node('tools',ToolNode(tools))
+    graph.add_edge(START,'chatbot')
+    graph.add_conditional_edges('chatbot',tools_condition)
+    graph.add_edge('tools',END)
+    return graph.compile(checkpointer=memory)
 
 agent = get_agent()
 config = {"configurable": {"thread_id": 1}}
-userinput = st.text_input("enter user input...")
 
+query = st.text_input("Enter your Query...")
 if st.button("send"):
-    if userinput is not None:
-        query = {"human_message" : [HumanMessage(content=userinput)]}
-        response = agent.invoke(query,config=config)
-        st.write(response["ai_message"][-1].content)
-
-
-# query = {"human_message" : [HumanMessage(content="what is my name")]}
-# response = agent.invoke(query,config=config)
-# print(response["ai_message"][-1].content)
+    if query is not None:
+        query = {'messages':[HumanMessage(content=query)]}
+        res = agent.invoke(query,config=config)
+        st.write(res['messages'][-1].content)
